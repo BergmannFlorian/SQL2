6e5812c98c3b6759eb74ae023e1b4075112c7660
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-12-18 13:47:15 +0100

    add error on frequent customer function

--- procedures_functions/frequentCustomers.sql
+++ procedures_functions/frequentCustomers.sql
@@ -14,31 +14,42 @@ BEGIN
     DECLARE @date1 AS DATETIME
     DECLARE @date2 AS DATETIME
 
-    DECLARE cClients CURSOR FOR SELECT DISTINCT lastname, firstname FROM Booking WHERE dateBooking BETWEEN @dateStart AND @dateEnd
-    OPEN cClients
-    
-    FETCH cClients INTO @lastname, @firstname
-    WHILE (@@FETCH_STATUS = 0 ) BEGIN
-        SET @inserted = 'false'
-        DECLARE cBoockings CURSOR FOR SELECT idBooking FROM Booking WHERE lastname = @lastname AND firstname = @firstname
-        OPEN cBoockings
-        FETCH cBoockings INTO @idBoocking
-
-        WHILE (@@FETCH_STATUS=0 AND @inserted = 'false') BEGIN
-            SET @date1 = (SELECT dateBooking FROM booking WHERE idBooking = @idBoocking)
-            SET @date2 = (SELECT dateBooking FROM booking WHERE idBooking = @idBoocking1)
-            
-            IF(DATEDIFF(day, @date1, @date2) <= @frequence) BEGIN
-                INSERT INTO @FrequentCustomers (Firstname, Lastname) VALUES (@firstname, @lastname)
-                SET @inserted = 'true'
-            END
-            SET @idBoocking1 = @idBoocking
-            FETCH cBoockings INTO @idBoocking
-        END
+    IF(@frequence <= 0) BEGIN
+        INSERT INTO @FrequentCustomers (error) VALUES ('Pas possible pour un jour négatif')
+    END
+    IF(@frequence > DATEDIFF(day, @dateStart, @dateEnd)) BEGIN
+        INSERT INTO @FrequentCustomers (error) VALUES ('Internval plus grand que la durée réel')
+    END
+    IF(@dateStart >= @dateEnd) BEGIN
+        INSERT INTO @FrequentCustomers (error) VALUES ('La date de départ doit être plus petite que la date de fin')
+    END
+    IF( (SELECT COUNT(*) FROM @FrequentCustomers) = 0) BEGIN
+        DECLARE cClients CURSOR FOR SELECT DISTINCT lastname, firstname FROM Booking WHERE dateBooking BETWEEN @dateStart AND @dateEnd
+        OPEN cClients    
 
         FETCH cClients INTO @lastname, @firstname
-        CLOSE cBoockings
-        DEALLOCATE cBoockings
+        WHILE (@@FETCH_STATUS = 0 ) BEGIN
+            SET @inserted = 'false'
+            DECLARE cBoockings CURSOR FOR SELECT idBooking FROM Booking WHERE lastname = @lastname AND firstname = @firstname
+            OPEN cBoockings
+            FETCH cBoockings INTO @idBoocking
+
+            WHILE (@@FETCH_STATUS=0 AND @inserted = 'false') BEGIN
+                SET @date1 = (SELECT dateBooking FROM booking WHERE idBooking = @idBoocking)
+                SET @date2 = (SELECT dateBooking FROM booking WHERE idBooking = @idBoocking1)
+            
+                IF(DATEDIFF(day, @date1, @date2) <= @frequence) BEGIN
+                    INSERT INTO @FrequentCustomers (Firstname, Lastname) VALUES (@firstname, @lastname)
+                    SET @inserted = 'true'
+                END
+                SET @idBoocking1 = @idBoocking
+                FETCH cBoockings INTO @idBoocking
+            END
+
+            FETCH cClients INTO @lastname, @firstname
+            CLOSE cBoockings
+            DEALLOCATE cBoockings
+        END
     END
     RETURN
 END
